1923f08461b9226e83dae608360cfc38afbb1a84
MasahikoYonezawa <y.masahiko0802@gmail.com>
2022-11-08 00:04:16 +0900

    feat: atomic design

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,6 @@
-import { BrowserRouter, Link } from 'react-router-dom';
-import { Router } from './router/Router';
+import './styles.css';
+import { Router } from './Router/Router';
 
 export const App = () => {
-    return (
-        <BrowserRouter>
-            <div className="App">
-                <Link to="/">Home</Link>
-                <br />
-                <Link to="/page1">Page1</Link>
-                <br />
-                <Link to="/page2">Page2</Link>
-            </div>
-            <Router></Router>
-        </BrowserRouter>
-    );
+    return <Router />;
 };
